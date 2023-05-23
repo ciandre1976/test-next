c61a4a16353035d983c3a68bb8f3532ee807d22d
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-05-23 17:08:06 +0200

    fix

--- src/comps/Header.tsx
+++ src/comps/Header.tsx
@@ -1,15 +1,25 @@
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <div>
+    <div className="flex justify-between m  -2 max-w-6xl">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <div></div>
+      <div>
+        <Link href="/">
+          <h2 className="text-2xl ">
+            <span className="bg-amber-500 font-bold px-2 py-1 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
     </div>
   );
 }
