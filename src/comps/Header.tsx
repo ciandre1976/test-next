e4a363b609a4c0fd393c4340027b293765e2ccd7
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-05-23 15:55:22 +0200

    menu

---
+++ src/comps/Header.tsx
@@ -0,0 +1,13 @@
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+      </div>
+      <div></div>
+    </div>
+  );
+}
